98ad32fe3a74ccc98ac3bd4d94acea14c95d6ad4
Mitul <pipaliyamitul12@gmail.com>
2022-12-05 08:18:54 -0800

    Add project files.

---
+++ BBranch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Xml.Linq;
+
+class BBranch
+{  
+    public class Branch
+    {        
+        public List<Branch> branches = new List<Branch>();        
+    };
+   
+
+    public static int Depth(Branch root, int depth)
+    {
+        int result = depth + 1;
+
+        foreach (var node in root.branches)
+            result = Math.Max(result, Depth(node, depth + 1));
+
+        return result;
+    }
+
+   
+    public static void Main()
+    {
+        Branch root = new Branch();
+        (root.branches).Add(new Branch());
+        (root.branches).Add(new Branch());
+        (root.branches[0].branches).Add(new Branch());
+        (root.branches[1].branches).Add(new Branch());
+        (root.branches[1].branches).Add(new Branch());
+        (root.branches[1].branches).Add(new Branch());
+        (root.branches[1].branches[0].branches).Add(new Branch());
+        (root.branches[1].branches[1].branches).Add(new Branch());
+        (root.branches[1].branches[1].branches).Add(new Branch());
+        (root.branches[1].branches[2].branches).Add(new Branch());
+        (root.branches[1].branches[1].branches[0].branches).Add(new Branch());
+
+
+        int result = Depth(root,0);
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
+
